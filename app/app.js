42c695cf194ba4d25008b22968147f16427139b8
Alena Hrytsuk <Alena_Hrytsuk@epam.com>
2018-03-22 01:22:21 +0300

    added date for todos, implemented delete operation for them, sorting by title and date, filtering by number of days

--- app/app.js
+++ app/app.js
@@ -8,21 +8,29 @@ angular.module('AngularApp', [
         ];
 
         $scope.todos = [
-            {"id": 0, "title": "Hit the gym", "category": "Done todos" },
-            {"id": 1, "title": "Bye eggs", "category": "Done todos" },
-            {"id": 2, "title": "Read you dont know JS book", "category": "New todos" },
-            {"id": 3, "title": "Not getting enough sleep", "category": "Done todos" },
-            {"id": 4, "title": "Finish frontcamp and survive", "category": "New todos" },
-            {"id": 5, "title": "Start read JS book", "category": "Done todos" },
-            {"id": 6, "title": "Get a promotion",  "category": "New todos" },
-            {"id": 7, "title": "go snowboarding",  "category": "Done todos" },
-            {"id": 8, "title": "Stay alive till Friday",  "category": "New todos" }
+            {"id": 0, "title": "Hit the gym", "category": "Done todos", "date": new Date(2018, 00, 22) },
+            {"id": 1, "title": "Bye eggs", "category": "Done todos", "date": new Date(2018, 01, 12) },
+            {"id": 2, "title": "Read you dont know JS book", "category": "New todos", "date": new Date(2017, 01, 22) },
+            {"id": 3, "title": "Not getting enough sleep", "category": "Done todos", "date": new Date(2018, 02, 05) },
+            {"id": 4, "title": "Finish frontcamp and survive", "category": "New todos", "date": new Date(2018, 02, 10) },
+            {"id": 5, "title": "Start read JS book", "category": "Done todos", "date": new Date(2018, 02, 24) },
+            {"id": 6, "title": "Get a promotion",  "category": "New todos", "date": new Date(2018, 03, 11) },
+            {"id": 7, "title": "go snowboarding",  "category": "Done todos", "date": new Date(2018, 01, 28) },
+            {"id": 8, "title": "Stay alive till Friday",  "category": "New todos", "date": new Date(2018, 02, 17) }
         ];
 
         $scope.currentCategory = null;
         $scope.isCreating = false;
         $scope.isEditing = false;
         $scope.editedTodo = null;
+        $scope.searchQuery   = '';
+
+        $scope.sortType     = 'title';
+        $scope.sortReverse  = false;
+
+        $scope.filterDate=function(obj){
+          return Math.round( (new Date() - obj.date) / (1000*60*60*24)) >= $scope.searchQuery;
+        }
 
         function isCurrentCategory(category) {
             return $scope.currentCategory !== null && category.name === $scope.currentCategory.name;
@@ -40,7 +48,7 @@ angular.module('AngularApp', [
         function shouldShowCreating() {
             return $scope.currentCategory && !$scope.isEditing;
         }
-        //
+
         function startCreating() {
             $scope.isCreating = true;
             $scope.isEditing = false;
@@ -59,7 +67,7 @@ angular.module('AngularApp', [
         function shouldShowEditing() {
             return $scope.isEditing && !$scope.isCreating;
         }
-        //
+
         function startEditing() {
             $scope.isCreating = false;
             $scope.isEditing = true;
@@ -78,7 +86,8 @@ angular.module('AngularApp', [
       function resetCreateForm() {
           $scope.newTodo = {
               title: '',
-              category: $scope.currentCategory.name
+              category: $scope.currentCategory.name,
+              date: new Date()
           };
       }
 
@@ -110,9 +119,6 @@ angular.module('AngularApp', [
           resetCreateForm();
       }
 
-      $scope.createTodo = createTodo;
-
-
       function updateTodo(todo) {
           var index = _.findIndex($scope.todos, function (b) {
               return b.id == todo.id
@@ -123,7 +129,15 @@ angular.module('AngularApp', [
           $scope.isEditing = false;
       }
 
-      $scope.updateTodo = updateTodo;
+      function deleteTodo(todo) {
+          _.remove($scope.todos, function (b) {
+              return b.id == todo.id;
+          });
+      }
 
+
+      $scope.updateTodo = updateTodo;
+      $scope.createTodo = createTodo;
+      $scope.deleteTodo = deleteTodo;
     })
 ;
